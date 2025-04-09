bd53e2bc1362f0c77b237f2bca0e2639f166fc1d
Pedro Cabrera <pedrocabrera@Pedros-MacBook-Pro.local>
2025-04-08 21:35:51 -0400

    Cambiando a HashRouter para GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Cuestionario from './pages/Cuestionario';
 import Campanas from './pages/Campanas';
@@ -10,7 +10,7 @@ import './App.css';
 
 function App() {
   return (
-    <Router basename="/fepasa-app">
+    <Router>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/cuestionario" element={<Cuestionario />} />
