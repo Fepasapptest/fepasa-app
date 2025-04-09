c36df9a21b0e8f508e84c939d6c0f3324178060d
Pedro Cabrera <pedrocabrera@Pedros-MacBook-Pro.local>
2025-04-08 22:07:14 -0400

    fix: Corregir rutas de archivos estáticos

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
+import '../App.css';
 
 const Header: React.FC = () => {
   return (
     <header className="header">
       <img 
-        src="/logo.png" 
+        src="logo.png" 
         alt="Logo FEPASA" 
         className="logo"
-        style={{ maxHeight: '60px' }}
+        style={{ 
+          maxHeight: '60px',
+          width: 'auto',
+          display: 'block'
+        }}
       />
     </header>
   );
